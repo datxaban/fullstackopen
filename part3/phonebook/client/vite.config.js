5180c8489634ab789454d406d673f01b078c53ce
Dat Doan <dathaibamai@gmail.com>
2024-06-02 22:24:14 +0300

    proxy config

--- part3/phonebook/client/vite.config.js
+++ part3/phonebook/client/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://fullstackopen-phonebook-60u4.onrender.com',
+        changeOrigin: true,
+      },
+    }
+  },
 })
